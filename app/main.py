97d0809fef529a951a554fa71c017b12a21cf4ba
James Wong <jcwong86@gmail.com>
2014-03-27 12:45:55 -0400

    Oops - undo that last one.
    That didn't work.

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import test_variables
 import import_gtfs
 import output_files
 import calculate_metrics
--- app/test_variables.py
+++ app/test_variables.py
@@ -1,8 +1,23 @@
 import psycopg2
 
-global bart = "http://gtfs.s3.amazonaws.com/bart-archiver_20140109_0110.zip"
-global mta ="http://gtfs.s3.amazonaws.com/mta-new-york-city-transit_20140205_0118.zip"
-global mbta = "http://gtfs.s3.amazonaws.com/massachusetts-archiver_20140315_0110.zip"
+global bart
+global mta
+global mbta
+global james_db
+global drew_db
 
-global james_db = psycopg2.connect(hostname="localhost", database="db", user="postgres", password="password")
-global drew_db = psycopg2.connect(hostname="localhost", database="db", user="postgres", password="password")
+bart = "http://gtfs.s3.amazonaws.com/bart-archiver_20140109_0110.zip"
+mta ="http://gtfs.s3.amazonaws.com/mta-new-york-city-transit_20140205_0118.zip"
+mbta = "http://gtfs.s3.amazonaws.com/massachusetts-archiver_20140315_0110.zip"
+
+try:
+    james_db = psycopg2.connect(host="localhost", database="db", user="postgres", password="Honduras")
+    print "Connected to James's database. Use james_db for object name."
+except:
+    print "James's database not connected."
+    
+try:
+    drew_db = psycopg2.connect(host="localhost", database="db", user="postgres", password="password")
+    print "Connected to Drew's database. Use drew_db for object name."
+except:
+    print "Drew's database not connected."
